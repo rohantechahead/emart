f307810f750e6e0aba44cea098428446d31d09af
rohan.bokadiya <rohan.bokadiya@techaheadcorp.com>
2024-10-19 13:17:36 +0530

    adding common for each services

---
+++ services/user_service/common/constant_helper.py
@@ -0,0 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+print("Database URL:-----> ", DATABASE_URL)
+SECRET_KEY = os.getenv("SECRET_KEY")
+STATIC_OTP = os.getenv("STATIC_OTP")
+SECRET_KEY_TOKENS = os.getenv("SECRET_KEY_TOKENS")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+REFRESH_TOKEN_EXPIRE_DAYS = os.getenv("REFRESH_TOKEN_EXPIRE_DAYS")
+
+DEBUG = True if os.getenv("DEBUG") == "True" else False
---
+++ services/user_service/common/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .constant_helper import DATABASE_URL
+
+DATABASE_URL = DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Dependency for getting DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
