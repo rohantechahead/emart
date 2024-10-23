fe7148e31ae2383143490525bdb7bf0d517867f6
tanuguptaaaa <tanugupta8420@gmail.com>
2024-10-23 12:13:23 +0530

    add_cart

--- services/order_service/app/routes.py
+++ services/order_service/app/routes.py
@@ -1,29 +1,17 @@
 from typing import List
 from fastapi import Depends, APIRouter
-from sqlalchemy import func
 from sqlalchemy.orm import Session
 from common.database import get_db
-from services.order_service.app import models
-from services.order_service.app.models import Cart
 from services.order_service.app.schemas import CartItemCreate, CartItem
-from services.order_service.app.services import get_current_user_id, get_user_cart_items, remove_cart_item
+from services.order_service.app.services import get_current_user_id, get_user_cart_items, remove_cart_item, \
+    add_or_update_cart_item
 
 router = APIRouter()
 
 
 @router.post("/add-to-cart", response_model=CartItem)
 def add_to_cart(item: CartItemCreate, db: Session = Depends(get_db), user_id: int = Depends(get_current_user_id)):
-    cart_item = db.query(Cart).filter(Cart.user_id == user_id,
-                                      Cart.product_id == item.product_id).first()
-    if cart_item:
-        cart_item.quantity += item.quantity
-        cart_item.updated_at = func.now()
-    else:
-        cart_item = models.Cart(user_id=user_id, product_id=item.product_id, quantity=item.quantity)
-        db.add(cart_item)
-
-    db.commit()
-    db.refresh(cart_item)
+    cart_item = add_or_update_cart_item(db, user_id, item.product_id, item.quantity)
     return cart_item
 
 @router.get("/view-cart", response_model=List[CartItem])
--- services/order_service/app/services.py
+++ services/order_service/app/services.py
@@ -2,10 +2,26 @@ from typing import Optional, List
 import jwt
 from fastapi import HTTPException, Header
 from jwt import ExpiredSignatureError
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
 from common.constant_helper import SECRET_KEY_TOKENS, ALGORITHM
 from services.order_service.app.models import Cart
+
+
+def add_or_update_cart_item(db: Session, user_id: int, product_id: int, quantity: int):
+    cart_item = db.query(Cart).filter(Cart.user_id == user_id, Cart.product_id == product_id).first()
+
+    if cart_item:
+        cart_item.quantity += quantity
+        cart_item.updated_at = func.now()
+    else:
+        cart_item = Cart(user_id=user_id, product_id=product_id, quantity=quantity)
+        db.add(cart_item)
+
+    db.commit()
+    db.refresh(cart_item)
+    return cart_item
 
 
 def get_current_user_id(authorization: Optional[str] = Header(None)):
